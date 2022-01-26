40394cfdb285d3714b71bdbe541e13924c6978a3
GGahagan27 <71200016+GGahagan27@users.noreply.github.com>
2022-01-26 10:06:36 -0800

    Completed Demonstration

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function selectPlan( planName, monthlyPrice){
+    console.log(planName, monthlyPrice)
+
+    const planElement = document.getElementById('planname');
+    planElement.innerHTML = planName; 
+
+    const monthlyElement = document.getElementById('monthlyrate');
+    monthlyElement.innerHTML = "$" + monthlyPrice + "/mo";
+
+    const yearlyElement = document.getElementById('annualrate');
+    yearlyElement.innerHTML = "$" + (monthlyPrice*12) + "/mo";
+
+    let nextYear = new Date(); 
+    nextYear.setFullYear( nextYear.getFullYear() + 1)
+    const gooduntilElement = document.getElementById('gooduntil');
+    gooduntilElement.innerHTML = nextYear.toDateString ();
+   
+}
